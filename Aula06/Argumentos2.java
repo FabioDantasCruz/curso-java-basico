fb8980223a4552f900190b379019c714ca50c001
Fabio <Fabio.dantascruz@hotmail.com>
2020-02-22 19:39:50 -0300

    Alo mundo com passagem de argumento

---
+++ Aula06/Argumentos2.java
@@ -0,0 +1,13 @@
+package exercicios.Aula06;
+
+public class Argumentos2 {
+
+	// Exibir um alo mundo com passagem de argumento
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Alo mundo" + args[0]);
+
+	}
+}
