1b9c4d92e042a3581bea9d1646ce3a2380127652
Fabio <Fabio.dantascruz@hotmail.com>
2020-02-22 19:37:34 -0300

    Alo Mundo Simples

---
+++ Aula05/AloMundo.java
@@ -0,0 +1,15 @@
+package exercicios.Aula5;
+
+public class AloMundo {
+
+	
+	// Exibir um alo mundo simples
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Alo mundo");
+
+	}
+
+}
